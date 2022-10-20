a14f23c86df51f6cb88510cef04c7e925b0f5503
Alessandro Benetton <aleben98@gmail.com>
2022-10-20 13:04:54 +0200

    [master] First cube render

---
+++ public/JS/JSTools/Camera.js
@@ -0,0 +1,109 @@
+let updateCamera = true;
+// Parametri globali utilizzati all'interno di Camera.js.
+//
+let drag;
+let THETA = degToRad(170),
+	PHI = degToRad(40);
+let old_x, old_y;
+let dX, dY;
+
+//
+let radius = 24;
+let maxRadius = 24,
+	minRadius = 3;
+
+// Definizione della classe "Camera".
+// A suo interno vi è la completa gestione delle caratteristiche relative
+// alla camera.
+export class Camera {
+	// Costruttore della classe "Camera".
+	// position, posizione spaziale (x, y, z) della camera.
+	// up, ...
+	// target, soggetto della scena.
+	// fieldOfView, ...
+	constructor(position, up, target, fieldOfView) {
+		this.position = position;
+		this.up = up;
+		this.target = target;
+		this.fieldOfView = fieldOfView;
+	}
+
+	radiusModify(radius) {
+		return radius * Math.cos(PHI);
+	}
+
+	moveCamera() {
+		console.log("Camera update");
+		this.position[0] = Math.cos(THETA) * this.radiusModify(radius);
+		this.position[1] = Math.sin(THETA) * this.radiusModify(radius);
+		this.position[2] = Math.sin(PHI) * radius;
+		updateCamera = false;
+	}
+
+	// Compute the camera's matrix using look at.
+	cameraMatrix() {
+		return m4.lookAt(this.position, this.target, this.up);
+	}
+
+	// Make a view matrix from the camera matrix.
+	viewMatrix() {
+		return m4.inverse(this.cameraMatrix());
+	}
+
+	// Compute the projection matrix
+	projectionMatrix(gl) {
+		let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+		return m4.perspective(this.fieldOfView, aspect, 1, 2000);
+	}
+}
+
+export function getUpdateCamera() {
+	return updateCamera;
+}
+
+export function setCameraControls(canvas, isActive) {
+	window.addEventListener("keydown", onKeyDown, true);
+
+	canvas.onmousedown = function (e) {
+		drag = true;
+		old_x = e.pageX;
+		old_y = e.pageY;
+		e.preventDefault();
+		return false;
+	};
+
+	canvas.onmouseup = function (e) {
+		drag = false;
+	};
+
+	canvas.onmousemove = function (e) {
+		if (!drag) return false;
+		updateCamera = true;
+		dX = (-(e.pageX - old_x) * 2 * Math.PI) / canvas.width;
+		dY = (-(e.pageY - old_y) * 2 * Math.PI) / canvas.height;
+		THETA += dX;
+		PHI -= dY;
+		if (PHI > degToRad(75)) PHI = degToRad(75);
+		if (PHI < degToRad(35)) PHI = degToRad(35);
+		old_x = e.pageX;
+		old_y = e.pageY;
+		e.preventDefault();
+	};
+
+	// Lista codici - tastiera
+	// 87 >> " W "
+	// 83 >> " S "
+	function onKeyDown(e) {
+		if (isActive) {
+			// console.log("Rilevato un movimento del raggio della camera.");
+			if (e.keyCode === 87 && radius > minRadius) radius -= 1;
+			else if (e.keyCode === 83 && radius < maxRadius) radius += 1;
+			// console.log(radius, maxRadius, minRadius);
+			updateCamera = true;
+		}
+	}
+}
+
+function degToRad(d) {
+	return (d * Math.PI) / 180;
+}
---
+++ public/JS/JSTools/Scene.js
@@ -0,0 +1,61 @@
+
+// Note:	In Javascript, the "export" keyword permits to export the class to other files.
+//			When we export the class, we can use all the methods and properties of the class in other 
+//			files.
+export class Scene {
+
+	/**
+	 * Costuctor of the class.
+	 * 
+	 * @param {string} name - The name of the scene.
+	 * @param {object} sceneObj - An array of objects that will be rendered in the scene.
+	 */
+	constructor(name = "defaultSceneComposition", sceneObj = []) {
+		this.sceneName;
+		this.sceneObj = sceneObj;
+	}
+
+	/**
+	 * Add an object to the scene.
+	 * 
+	 * @param {String} alias A string that will be used to identify the object
+	 * @param {String} pathOBJ The path to the .obj file
+	 * @param {object} coords An object that contains the coordinates of the object inside the scene
+	 * 
+	 * @returns {boolean} True if the object has been added, false otherwise
+	 */
+	addOBJToList(alias, pathOBJ, coords) {
+		var newObj = {
+			alias: alias,
+			pathOBJ: pathOBJ,
+			coords: coords,
+		};
+		if (this.sceneObj.push(newObj)) {
+			console.log("Scene.js - Added new object to the scene");
+			return true;
+		} else {
+			console.log("Scene.js - Error while adding new object to the scene");
+			console.debug(newObj);
+			return false;
+		}
+	}
+
+	/**
+	 * Remove an object from the scene.
+	 * 
+	 * @param {string} alias - The alias of the object that will be removed
+	 * 
+	 * @returns {boolean} - True if the object has been removed, false otherwise
+	 * 
+	 */
+	removeOBJFromList(alias) {
+		for (var i = 0; i < this.sceneObj.length; i++) {
+			if (this.sceneObj[i].alias === alias) {
+				this.sceneObj.splice(i, 1);
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
