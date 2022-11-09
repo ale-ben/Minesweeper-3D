3fed488a91c9143af6a7264aece21c999ad84368
Alessandro Benetton <aleben98@gmail.com>
2022-11-09 17:58:33 +0100

    [master] Test MTL selector

--- public/resources/js/main.js
+++ public/resources/js/main.js
@@ -5,7 +5,8 @@ async function main() {
 	
 	const env = new Environment("#canvas");
 
-	await env.addObject(new ObjectRenderer("Cube", './resources/models/cube.obj', {x: 0, y: 0, z: 1}));
+	await env.addObject(new ObjectRenderer("Cube", './resources/models/cube.obj', {x: 0, y: 0, z: 0}, './resources/models/cubeTextured.mtl'));
+	await env.addObject(new ObjectRenderer("Cube", './resources/models/cube.obj', {x: 0, y: 0, z: 2}));
 	//await env.addObject(new ObjectRenderer("Boeing", './resources/models/boeing.obj'));
 	//await env.addObject(new ObjectRenderer("Chair", './resources/models/chair.obj', {x: -5, y: -5, z: 0}));
 	await env.addObject(new ObjectRenderer("Axes", './resources/models/axes.obj'));
