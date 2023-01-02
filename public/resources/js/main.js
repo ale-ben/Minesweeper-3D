21a48e59a39257a6f4bf123fb9046058a2ff6c7f
Alessandro Benetton <aleben98@gmail.com>
2023-01-02 12:38:59 +0100

    [master] Nothing to see here

--- public/resources/js/main.js
+++ public/resources/js/main.js
@@ -8,19 +8,15 @@ import {
 async function main() {
     const env = new Environment("#canvas");
 
-    await env.addObject(new Element("Cube", "./resources/models/cube.obj", null, {
-        x: 0,
-        y: 0,
-        z: 0.5
-    }, true, "./resources/models/cubeTextured.mtl"));
+
+    await env.addObject(new Element("Axes", "./resources/models/axes.obj"));
+
+    await env.addObject(new Element("Cube", "./resources/models/cube.obj", null, null, true, "./resources/models/cubeTextured.mtl"));
     await env.addObject(new Element("Cube", "./resources/models/cube.obj", {
         x: 0,
         y: 0,
         z: 2
     }));
-    //await env.addObject(new Element("Boeing", './resources/models/untitled.obj'));
-    //await env.addObject(new Element("Chair", './resources/models/chair.obj', {x: -5, y: -5, z: 0}));
-    await env.addObject(new Element("Axes", "./resources/models/axes.obj"));
 
     function render(time) {
         time *= 0.001; // convert to seconds
